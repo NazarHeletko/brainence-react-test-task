88a52c7de04df0a20b0f9d6a1742cfcc65dd62ec
Nazar <somewhereseen@gmail.com>
2020-03-25 23:22:23 +0200

    Updates

---
+++ precache-manifest.a48be1e5a2e4bdd04ee9851cb247d6f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eb4de1b9d798b3ee9a1ed980e9c8d04",
+    "url": "/brainence-react-test-task/index.html"
+  },
+  {
+    "revision": "da2a3e77e1db2c171caa",
+    "url": "/brainence-react-test-task/static/css/main.37023f30.chunk.css"
+  },
+  {
+    "revision": "a491eb4847404a95e3f2",
+    "url": "/brainence-react-test-task/static/js/2.1ca613c8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/brainence-react-test-task/static/js/2.1ca613c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da2a3e77e1db2c171caa",
+    "url": "/brainence-react-test-task/static/js/main.dde23457.chunk.js"
+  },
+  {
+    "revision": "c9790ed5315b5c415367",
+    "url": "/brainence-react-test-task/static/js/runtime-main.2df12c7a.js"
+  }
+]);
